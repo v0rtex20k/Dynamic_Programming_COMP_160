a8e15d135afeae181e2624c63e7e6cb472bd43b5
Victor <vitek@resnet142-42.medford.tufts.edu>
2019-11-10 23:55:51 -0500

    final

--- MSS.py
+++ MSS.py
@@ -12,11 +12,11 @@ def maxSub(arr, start, end):
 		return 0
 	if start == end:
 		return arr[start]
-	return arr[start] + maxSub(arr, start+1, end)
+	return max(arr[start], arr[start] + maxSub(arr, start+1, end))
 
 if __name__ == '__main__':
-	arr = (20, -20, 3, -1, 5, 20, 10, -2, 12, 14)
+	arr = (-3, -1, 5)
 	for i in range(1, len(arr)):
 		maxSub(arr, 0, len(arr) - i)
 	max_score= max(maxima, key=lambda k: maxima[k])
-	print(len(arr), " - (" + str(max_score[1]) + ", " + str(max_score[2]) + "):", maxima[max_score])
+	print("(" + str(max_score[1]) + ", " + str(max_score[2]) + "):", maxima[max_score])
